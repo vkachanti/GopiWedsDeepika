982134ebe439f015233be94b56dd410aa791d3bb
Achanti Vamsi Krishna <151735259+vkachanti@users.noreply.github.com>
2025-02-19 19:43:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ function searchGoogle() {
     }
 }
 
-function feelingLucky() {
-    let query = document.getElementById("search-box").value;
-    if (query) {
-        window.location.href = `https://www.google.com/search?q=${encodeURIComponent(query)}&btnI=1`;
-    }
+function redirectToResults() {
+    window.location.href = "resultgo.html";
 }
